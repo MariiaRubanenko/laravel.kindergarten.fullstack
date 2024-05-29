500d3c408422c7bd5f066a641ff3093c2f220f9b
Nikita Lisovyi <nykyta.lisovyi@nure.ua>
2024-05-29 21:57:00 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -18,9 +18,10 @@ return [
     // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'paths' => ['*'],
-    'allowed_methods' => ['*'],
+    
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:8081'],
 
     'allowed_origins_patterns' => [],
 
